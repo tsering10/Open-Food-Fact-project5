a1bf6ecc08c080e8af8a7b7c361380e7db5bd3ee
tashi tsering <tashi.cse@gmail.com>
2020-07-24 10:31:18 +0200

    test.py removed

--- test.py
+++
@@ -1,9 +0,0 @@
-from configparser import ConfigParser
-filename='config.ini'
-parser = ConfigParser()
-parser.read(filename, encoding="utf-8")
-section = "api"
-db = {}
-if parser.has_section(section):
-    items = parser.items(section)
-    print(items)
