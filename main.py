425efd9c88119af155c5f403573c717f9e9a5ccb
Tashi Tsering <48253465+tsering10@users.noreply.github.com>
2020-07-31 12:13:28 +0200

    update main.py conforming pep8

--- main.py
+++ main.py
@@ -1,82 +1,56 @@
-from models.model import *
-from sqlalchemy.ext.declarative import declarative_base
-from queries import display_categories,display_products,Get_product_store,display_better,insert_favorite,display_favorite
+from queries import display_categories, display_products, get_product_store, display_better, insert_favorite, \
+    display_favorite
 from DBLoad import sess
-from flask import Flask, render_template, request, flash,session
-from flask import redirect
+from flask import Flask, render_template, request, flash, session
 
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 
-@app.route("/") 
+@app.route("/")
 def home():
     return render_template('index.html')
 
-@app.route("/category",methods=['GET', 'POST']) 
-def categorys():
-    dico = display_categories(sess)
-    
-    return render_template('category.html',dico=dico)
+
+@app.route("/category", methods=['GET', 'POST'])
+def category():
+    category_dic = display_categories(sess)
+    return render_template('category.html', dico=category_dic)
 
 
 @app.route("/product_category", methods=['GET', 'POST'])
-
 def products():
     # Get the selected category id
     cp = request.args.get('cat_id')
     session['catID'] = cp
     if 'catID' in session:
-        catID = session['catID']
-        all_product = display_products(sess,catID)
-        return render_template("products.html", all_product = all_product)
+        cat_id = session['catID']
+        all_product = display_products(sess, cat_id)
+        return render_template("products.html", all_product=all_product)
+
 
 @app.route("/recommendation", methods=['GET', 'POST'])
-
 def recommendation():
     if not session.get("catID") is None:
-        cID = session.get("catID")
+        c_id = session.get("catID")
         product_id = request.args.get("productID")
-        select_store = Get_product_store(sess,product_id)
-        better_product = display_better(sess,product_id,cID)
+        select_store = get_product_store(sess, product_id)
+        better_product = display_better(sess, product_id, c_id)
         if better_product is None or len(better_product) == 0:
-            return render_template("nobetterproduct.html",select_store=select_store)
+            return render_template("nobetterproduct.html", select_store=select_store)
         else:
-            return render_template("recommendation.html",select_store=select_store, better_product = better_product)
+            return render_template("recommendation.html", select_store=select_store, better_product=better_product)
 
 
-@app.route("/favorite",methods=['GET', 'POST']) 
+@app.route("/favorite", methods=['GET', 'POST'])
 def favorite():
-
-    # if not session.get("catID") is None:
-    #     cID = session.get("catID")
     barcode = request.args.get("barcode")
-    better_product = Get_product_store(sess,barcode)
+    better_product = get_product_store(sess, barcode)
     # calling function insert_favorite to save the recommended product in database
-    insert_favorite(sess,better_product)
-        
+    insert_favorite(sess, better_product)
     fav_products = display_favorite(sess)
-
-    return render_template("favorite.html",fav_products=fav_products)
- 
-   
-        
+    return render_template("favorite.html", fav_products=fav_products)
 
 
-
-
-
-
-
-
-        
 if __name__ == "__main__":
     app.run(debug=True)
-    # db_engine = connect()
-    # file_name = '/home/tashitsering/Openfood project/open_food_data/open_food_data1.csv'
-    # dbfill = DBFeed(file_name)
-    # dbfill.populate_category()
-    # dbfill.populate_store()
-    # dbfill.populate_brand()
-    # dbfill.populate_product()
-
